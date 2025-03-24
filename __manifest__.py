cb073ed5a56e420ea2a3eb1af62a3e8f26698a7a
AbdulazizAmmar <azizammar26@gmail.com>
2025-03-24 18:34:25 +0300

    UPGRADE_18

--- __manifest__.py
+++ __manifest__.py
@@ -11,10 +11,10 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '13.1',
+    'version': '18.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale', 'stock', 'product', 'purchase', 'account', 'sales_customisations'],
+    'depends': ['base','sale', 'stock', 'product', 'purchase', 'account', 'sales_customisations', 'res_category'],
 
     # always loaded
     'data': [
